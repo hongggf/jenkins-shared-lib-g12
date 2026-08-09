44e7fecf9d4a14a4eab8c279a2cba3b160e98838
kkdev <kk@jenkins-machine.asia-southeast1-c.c.project-76d70acd-06f2-447f-954.internal>
2026-08-09 08:45:40 +0000

    function to scansonar and send telegram

--- vars/sendTelegram.groovy
+++ vars/sendTelegram.groovy
@@ -1,1 +1,10 @@
 // for sending message to telegram
+def call(String message, String token, String chatId){
+    // read the value from credentials ,so that can just send with the message
+    sh """
+     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="${chatId}" \
+        -d parse_mode="Markdown"  \
+        -d text="${message}"
+    """
+}
