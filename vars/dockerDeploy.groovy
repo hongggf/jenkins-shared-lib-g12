a21e1721809ab2cc77831c9a65297ff8d7b376ce
hongggf <kimlimhong2598@gmail.com>
2026-08-12 17:01:14 +0000

    fixe ddockerdeploy

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,12 +1,13 @@
 def call (String containerName, String imageName, int hostPort, int containerPort){
    { sh """
 
-        docker rm -f ${containerName} || true
-
+        docker rm -f ${containerName} 2>/dev/null || true
+        docker pull ${imageName}
         docker run -d \
             --name ${containerName} \
             -p ${hostPort}:${containerPort} \
             ${imageName}
+        docker ps
         """
         echo " container deployed successfully with name: ${containerName} and image: ${imageName} on port: ${hostPort}"    
 
