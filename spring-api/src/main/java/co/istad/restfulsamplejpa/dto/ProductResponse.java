9c5e9b23a76307a1e0d9194a06ac4090e7c5f015
hongggf <kimlimhong2598@gmail.com>
2026-08-13 11:45:32 +0000

    rm spring

--- spring-api/src/main/java/co/istad/restfulsamplejpa/dto/ProductResponse.java
+++
@@ -1,8 +0,0 @@
-package co.istad.restfulsamplejpa.dto;
-
-
-import lombok.Builder;
-
-@Builder
-public record ProductResponse(Long id , String title , String description , String imageUrl, float price ) {
-}
--- spring-api/src/main/java/co/istad/restfulsamplejpa/model/Owner.java
+++
@@ -1,4 +0,0 @@
-package co.istad.restfulsamplejpa.model;
-
-public class Owner {
-}
